60835c79e3f26433f70486e88aedaf6d6bc6b055
David <learnitmyway@users.noreply.github.com>
2022-02-22 14:26:23 +0100

    display secret messae value

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [secretMessageVal, setSecretMessageVal] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <label htmlFor="secret-message-input">Secret Message: </label>
+      <input
+        id="secret-message-input"
+        onChange={(e) => setSecretMessageVal(e.target.value)}
+        value={secretMessageVal}
+      />
+      <div>value: {secretMessageVal}</div>
+    </main>
   );
 }
 

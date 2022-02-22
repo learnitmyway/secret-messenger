04546e0e7e35a1ffc355e316d773859d4b0a34a3
David <learnitmyway@users.noreply.github.com>
2022-02-22 14:58:32 +0100

    add some more tests

--- src/util.test.js
+++ src/util.test.js
@@ -8,6 +8,21 @@ describe("Secret.util", () => {
     expect(decrypt({ encryptedMessage, key })).toBe(message);
   });
 
+  it("encrypts and decryption of an empty string", () => {
+    const message = "";
+    const key = hash({ str: "some key" });
+    const encryptedMessage = encrypt({ message, key });
+    expect(decrypt({ encryptedMessage, key })).toBe(message);
+  });
+
+  it("encrypt message twice", () => {
+    const message = "some message";
+    const key = hash({ str: "some key" });
+    const encryptedMessage = encrypt({ message, key });
+    const encryptedMessage2 = encrypt({ message, key });
+    expect(encryptedMessage).not.toBe(encryptedMessage2);
+  });
+
   describe("when the key is incorrect", () => {
     it("decrypts to empty string", () => {
       const message = "some message";
